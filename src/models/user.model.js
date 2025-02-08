a43af83ba2541616f935943d677943bd6186db45
Nandini317 <nandini3172004@gmail.com>
2025-02-09 00:47:57 +0530

    add user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,96 @@
+import mongoose , {Schema} from mongoose
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const userSchema = new Schema(
+    {
+        username : {
+            type:String , 
+            required:true  , 
+            unique:true,
+            lowercase:true ,
+            trim:true , 
+            index :true //jb bhi searching field enable krni hai to index:true krna better option hota hai 
+        },
+        email:{
+            type:String , 
+            required:true  , 
+            unique:true,
+            lowercase:true ,
+            trim:true , 
+        },
+        fullName : {
+            type:String , 
+            required:true  , 
+            index:true,
+            trim:true , 
+        } , 
+        avatar:{
+            type:String, // cloudnary ka use krenge ..3rd party app hai for uploading data 
+            required : true
+        } , 
+        coverImage : {
+            type:String , 
+            
+        },
+        watchHistory:[
+            {
+                type:Schema.Types.ObjectId , 
+                ref : "Video"
+            }
+        ] , 
+        password : {
+            type:String , 
+            required:[true ,'Password is required '  ], 
+        } ,
+        refreshToken :{
+            type:String
+        }
+        
+    },
+    {
+        timestamps : true 
+    }
+);
+
+userSchema.pre("save" ,async function (next){
+    if(!this.isModified("password")) return next() ; // agr password mein koi changes hi nahi hai to kyu hi update krna baar baar 
+    // like if someone changes avatar , then why to again hash the password ?
+
+    this.password = bcrypt.hash(this.password ,10 ) // hash(kisko krna hai , kitne rounds of salting )
+    next()
+} )
+
+//ham kuch methods define bhi kr skte hai .. like to check if the password entered by user is correct or not
+userSchema.methods.isPasswordCorrect = async function(password){
+   return await bcrypt.compare(password , this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id:this._id ,
+        email :this.email ,
+        username:this.username,
+        fullName : this.fullName
+    } ,
+    process.env.ACCESS_TOKEN_SECRET , 
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id:this._id ,
+        
+    } ,
+    process.env.REFRESH_TOKEN_SECRET , 
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+
+    )
+}
+
+export const User = mongoose.model("User" , userSchema);
---
+++ src/models/video.model.js
@@ -0,0 +1,49 @@
+import mongoose ,{Schema} from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+const videoSchema = new Schema(
+    {
+        videoFile : {
+            type : String , 
+            required :true , 
+        } , 
+        thumbnail : {
+            type : String , 
+            required :true , 
+        },
+        title : {
+            type : String , 
+            required :true , 
+        },
+        description: {
+            type : String , 
+            required :true , 
+        },
+        duration: {
+            type : Number , // from cloudnary (derive) 
+            required :true , 
+        },
+        view:{
+            type:Number,
+            default : 0 
+        },
+        ispublished : {
+            type : Boolean , 
+            default :true , 
+        },
+        owner :{
+            type : Schema.Types.ObjectId , 
+            ref : "User"
+        }
+
+    }
+    ,{timestamps : true }
+);
+
+mongoose.plugin(mongooseAggregatePaginate) 
+
+
+
+
+
+export const Video = mongoose.model("Video" , videoSchema)
