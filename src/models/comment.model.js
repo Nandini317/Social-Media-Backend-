290311e4bfacd0e0fe8dc591183485ee6e7556ec
Nandini317 <nandini3172004@gmail.com>
2025-03-17 01:14:52 +0530

    feat: add comment, like, playlist, and tweet models with relationships

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,7 +1,22 @@
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
 import mongoose ,{Schema} from "mongoose"
 
-const commentSchema = new Schema({} , {timestamps:true }) 
+const commentSchema = new Schema(
+    {
+        content :{
+            type:String , 
+            required : true ,
+        } ,
+        video: {
+            type : Schema.Types.ObjectId ,
+            ref : "Video"
+        } , 
+        owner: {
+            type : Schema.Types.ObjectId ,
+            ref : "User"
+        }
+    } , 
+    {timestamps:true }) 
 
 commentSchema.plugin(mongooseAggregatePaginate)
 export const Comment = mongoose.model("Comment" ,commentSchema )
