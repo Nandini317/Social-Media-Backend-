65f1906ae49a81f9793c0d56d08798cee35616af
Nandini <154619218+Nandini317@users.noreply.github.com>
2025-08-20 18:11:46 +0530

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -10,7 +10,7 @@ const userSchema = new Schema(
             unique:true,
             lowercase:true ,
             trim:true , 
-            index :true //jb bhi searching field enable krni hai to index:true krna better option hota hai 
+            index :true 
         },
         email:{
             type:String , 
@@ -26,7 +26,7 @@ const userSchema = new Schema(
             trim:true , 
         } , 
         avatar:{
-            type:String, // cloudnary ka use krenge ..3rd party app hai for uploading data 
+            type:String,  
             required : true
         } , 
         coverImage : {
@@ -53,28 +53,28 @@ const userSchema = new Schema(
     }
 );
 
-userSchema.pre("save" ,async function (next){ //fnctn should not be an arrow function , as we need the context of "this"
-    if(!this.isModified("password")) return next() ; // agr password mein koi changes hi nahi hai to kyu hi update krna baar baar 
-    // like if someone changes avatar , then why to again hash the password ?
+userSchema.pre("save" ,async function (next){ 
+    if(!this.isModified("password")) return next() ;  
+    
 
-    this.password = await bcrypt.hash(this.password ,10 ) // hash(kisko krna hai , kitne rounds of salting )
+    this.password = await bcrypt.hash(this.password ,10 )
     next()
 } )
 
-//ham kuch methods define bhi kr skte hai .. like to check if the password entered by user is correct or not
+
 userSchema.methods.isPasswordCorrect = async function(password){
    return await bcrypt.compare(password , this.password)
 }
 
 userSchema.methods.generateAccessToken = function(){
-    return jwt.sign( // (payload , secret key , expiration )
-    { //Payload (Data Inside the Token)
-        _id:this._id , //unique id 
+    return jwt.sign( 
+    { 
+        _id:this._id , 
         email :this.email ,
         username:this.username,
         fullName : this.fullName
     } ,
-    process.env.ACCESS_TOKEN_SECRET , //ensures only the server can generate valid tokens.
+    process.env.ACCESS_TOKEN_SECRET , 
     {
         expiresIn: process.env.ACCESS_TOKEN_EXPIRY
     }
