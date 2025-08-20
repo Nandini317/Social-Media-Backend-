b45695eeafe888a1fb7563237553b861d932aaa6
Nandini <154619218+Nandini317@users.noreply.github.com>
2025-08-20 18:07:48 +0530

    Update asyncHandler.js

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,3 @@
-// this is just a utility function to connect and talk with database ..as it is a repetitive task 
-// Two ways : 
-//              1. Try...catch
-//              2.  Promises
 
 
 const asyncHandler = (requestHandler)=>{
@@ -11,21 +7,3 @@ const asyncHandler = (requestHandler)=>{
 }
 export {asyncHandler}
 
-
-
-// const asyncHandler = ()=>{}
-// const asyncHandler = (func) =>{()=>{}}
-// const asyncHandler = (func) => ()=>{} 
-
-// const asyncHandler = (fn) => async(req, res, next)=>{
-//     try {
-//         await fn(req, res, next){
-
-//         }
-//     } catch (error) {
-//         res.status(error.code || 500).json({
-//             success:false ,
-//             message : error.message
-//         })
-//     }
-// }
