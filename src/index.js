578eef4352c0115ab400a7bddc1a130b6bc38ae3
Nandini <154619218+Nandini317@users.noreply.github.com>
2025-08-20 18:06:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import dotenv from "dotenv"
 import connectDB from './db/index.js';
 import {app} from './app.js'
 
-// 2nd approach : write the whole connection code in a seperate file(index.js in db) , and call it here ..since this is the main 
-// file , it is better approach to firsly import the environment bvariables 
 dotenv.config({
     path: './.env'
 })
@@ -24,30 +22,5 @@ connectDB() // this will return a promise
         console.log("MongoDB Connection Failed ! : index.js: ", err)
     })
 
-
-
-
-
-// 1st approach : every thing in index file : syntax used :function bnao aur immediately call krdo : ( ()=>{} )()
-
-// import express from "express"
-// import mongoose from 'mongoose'
-// import {DB_NAME} from './constants';
-// const app = express()
-// (async ()=>{
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         app.on("error" ,(error)=>{
-//             console.log("ERROR : " , error);
-//             throw error
-//         })
-//         app.listen(process.env.PORT , ()=>{
-//             console.log(`app is listening on post : ${process.env.PORT}`)
-//         })
-//     } catch (error) {
-//         console.error("ERROR : " , error)
-//         throw error
-//     }
-// })()
 
 
