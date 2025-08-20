6b05f6d4e2301aa4072980acc206361cffbe3fc6
Nandini <154619218+Nandini317@users.noreply.github.com>
2025-08-20 18:14:44 +0530

    Update auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,9 +5,7 @@ import {User} from "../models/user.model.js"
 
 export const verifyJWT = asyncHandler(async(req, res , next)=>{
     try {
-        const token = req.cookies?.accessToken ||req.header("Authorization")?.replace("Bearer " , "" ) // ho skta hai user request from se nehi header se aai ho 
-        // jb req header se aati hai , to content of header looks like : Authorization : Bearer <token> , so if we need token , then from header's authorizaton ky , just replace the "Bearer " , with "" so that we left with <token>
-    
+        const token = req.cookies?.accessToken ||req.header("Authorization")?.replace("Bearer " , "" ) 
         if(!token){
             throw new ApiError(401 , "Unauthorized request")
         }
