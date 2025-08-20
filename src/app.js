3ecdf31e44c0770a99dc3de4b3fdd7af6e926b31
Nandini <154619218+Nandini317@users.noreply.github.com>
2025-08-20 18:07:15 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,22 +5,16 @@ const app = express()
 
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN, //Specifies which origins (domains) are allowed to make cross-origin requests.
+    origin: process.env.CORS_ORIGIN, 
     credentials : true 
 }))
 
-// what is app.use() : function in Express.js adds middleware to the application’s request-processing pipeline. It applies the specified middleware to all incoming requests or to specific routes, allowing you to modify request/response objects, perform operations, or handle errors throughout the application.
-
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({
-    extended :true, //  allows nested objects in form data. eg : {user : age: 20, name :"manan"}
+    extended :true, 
     limit:"16kb"
 }))
-app.use(express.static("public")) // agr hm koi file pdf vgera sttore krna chahte hai , to ek public folder bna dete hai k public 
-//assest hai , koi bhi use krlo (jo hmne public folder bnaya hai)
-// so it is a middleware in Express.js that serves static files .When a request is made for a static file, Express automatically looks for it in the "public" folder.
-//Example: If you have public/style.css, you can access it via http://localhost:3000/style.css without needing to define a route.
-
+app.use(express.static("public")) 
 app.use(cookieParser())
 
 //routes  import 
