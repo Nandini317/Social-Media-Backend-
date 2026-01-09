c256db5a8a7c6cef52c3044b65e6cbbbcec0a069
Nandini <154619218+Nandini317@users.noreply.github.com>
2026-01-09 10:00:38 +0530

    update changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -322,7 +322,7 @@ const getUserCurrentProfile = asyncHandler(async(req , res)=>{
                 },
                 isSubscribed : {
                     $cond : {
-                        if : {$in : [req.user?._id , "$subscribers.subscriber"]} ,
+                        if : {$in : [req.user._id , "$subscribers.subscriber"]} ,
                         then:true , 
                         else : false 
 
